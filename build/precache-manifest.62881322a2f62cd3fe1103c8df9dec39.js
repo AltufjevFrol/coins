e5f6bdf9fa7af14ee0d9a7a336bdbe32014ad54a
Pilipenko Artem <artem.mihailovich@gmail.com>
2019-10-18 17:45:12 +0300

    build

---
+++ build/precache-manifest.62881322a2f62cd3fe1103c8df9dec39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07f3103f1c74a6cacc8539e24f35db6",
+    "url": "/coins/index.html"
+  },
+  {
+    "revision": "d5e2a3288f729815e6b7",
+    "url": "/coins/static/css/main.2fb6a694.chunk.css"
+  },
+  {
+    "revision": "7d741779dc8fd1d94302",
+    "url": "/coins/static/js/2.428469fc.chunk.js"
+  },
+  {
+    "revision": "d5e2a3288f729815e6b7",
+    "url": "/coins/static/js/main.092f958f.chunk.js"
+  },
+  {
+    "revision": "cba3e665c773c19ea0e7",
+    "url": "/coins/static/js/runtime-main.1781209c.js"
+  }
+]);
